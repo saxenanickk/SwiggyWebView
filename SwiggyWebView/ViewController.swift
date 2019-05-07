8c7518d5c669847aceee61da8a2328d495af96a3
Nikhil Saxena <saxenanickk@gmail.com>
2019-05-07 15:30:46 +0530

    Initial Commit

--- SwiggyWebView/ViewController.swift
+++ SwiggyWebView/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import WebKit
+class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler {
+    
+    @IBOutlet var containerView: UIView? = nil
+    var webView: WKWebView!
 
-class ViewController: UIViewController {
+    override func loadView() {
+        super.loadView()
+        
+        let contentController = WKUserContentController()
+        contentController.add(self, name: "setCookie")
+        contentController.add(self, name: "fireEvent")
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+        
+        webView = WKWebView(frame: .zero, configuration: config)
+        view = webView
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello, Welcome to Swiggy")
+        let myURL = URL(string:"https://www.swiggy.com/?utm_source=RMZ&utm_campaign=RMZ&platform=RMZ&token=61B667C88FB270F558FB9254F94A2D73")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
 
-
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {// edit: changed fun to func
+        if (message.name == "setCookie"){
+            print("Set Cookie: \(message)")
+        }
+        if (message.name == "fireEvent"){
+            print("Fire Event: \(message)")
+        }
+    }
 }
-
