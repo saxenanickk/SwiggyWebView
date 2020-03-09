70e50bcc1bfef056c87ece5d3dbf76d7aab1698f
Nikhil Saxena <saxenanickk@gmail.com>
2020-03-09 10:40:10 +0530

    Updated

--- SwiggyWebView/ViewController.swift
+++ SwiggyWebView/ViewController.swift
@@ -16,28 +16,41 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler {
     override func loadView() {
         super.loadView()
         
-        let contentController = WKUserContentController()
-        contentController.add(self, name: "setCookie")
-        contentController.add(self, name: "fireEvent")
-        let config = WKWebViewConfiguration()
-        config.userContentController = contentController
+        self.view.layoutMargins = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+     
+        let label = UILabel()
+        label.text = "Hello World"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
         
-        webView = WKWebView(frame: .zero, configuration: config)
-        view = webView
+        let margins = view.layoutMarginsGuide
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: margins.topAnchor)
+        ])
         
+//        let contentController = WKUserContentController()
+//        contentController.add(self, name: "setCookie")
+//        contentController.add(self, name: "fireEvent")
+//        let config = WKWebViewConfiguration()
+//        config.userContentController = contentController
+//
+//        webView = WKWebView(frame: .zero, configuration: config)
+//        webView.insetsLayoutMarginsFromSafeArea = true
+//
+//        view = webView
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello, Welcome to Swiggy")
-        let myURL = URL(string:"https://www.swiggy.com/?utm_source=RMZ&utm_campaign=RMZ&platform=RMZ&token=61B667C88FB270F558FB9254F94A2D73")
-        let myRequest = URLRequest(url: myURL!)
-        webView.load(myRequest)
+//        print("Hello, Welcome to Swiggy")
+//        let myURL = URL(string:"https://google.com/")
+//        let myRequest = URLRequest(url: myURL!)
+//        webView.load(myRequest)
     }
 
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {// edit: changed fun to func
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         if (message.name == "setCookie"){
-            print("Set Cookie: \(message)")
+            print("Set Cookie: \(message.body)")
         }
         if (message.name == "fireEvent"){
             print("Fire Event: \(message)")
